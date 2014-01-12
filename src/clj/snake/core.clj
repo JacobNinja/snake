2c7ee12811da35ea24ed704ad898ae3c149a32aa
Jacob Richardson <jacob.ninja.dev@gmail.com>
2014-01-12 13:51:24 -0700

    Refactor html script inclusion

--- src/clj/snake/core.clj
+++ src/clj/snake/core.clj
@@ -7,22 +7,31 @@
             [clojure.data.json :as json]
             [clojure.string :as string]))
 
-(defn page-for [env]
+(def dev
+  (vector
+   [:script {:src "js/out/goog/base.js"}]
+   [:script {:src "js/dev.js"}]
+   [:script "goog.require('snake.core');"]))
+
+(def prod
+  [[:script {:src "js/prod.js"}]])
+
+(defn page-for [scripts]
   (html [:head {:title "Snake"}
          [:link {:rel "stylesheet" :href "css/style.css"}]]
-        
-        [:body {:onload "snake.core.init();"}
-         [:div
-          [:div
-           "Level: " [:span {:id "level"} "1"]
-           [:canvas#world {:width 400 :height 400}]
-           [:script {:src  (str "js/" env ".js")}]]]]))
+        (vec (concat
+              [:body {:onload "snake.core.init();"}
+               [:div
+                [:div
+                 "Level: " [:span {:id "level"} "1"]
+                 [:canvas#world {:width 400 :height 400}]]]]
+              scripts))))
 
 (defroutes app-routes
   (GET "/" []
-       (page-for "prod"))
+       (page-for prod))
   (GET "/dev" []
-       (page-for "dev"))
+       (page-for dev))
   (route/resources "/")
   (route/not-found "Not found"))
 
